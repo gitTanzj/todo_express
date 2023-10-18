9116bb1a085a10c147b4c94603da461c04aa3dfa
gitTanzj <kalle.riit@gmail.com>
2023-10-18 13:29:42 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,15 +6,45 @@ const path = require('path')
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-app.get('/', (req, res) => {
-    fs.readFile('./tasks', 'utf8', (err, data) => {
-        if (err) {
-            console.err(err)
-            return;
-        }
-        const tasks = data.split('\n')
-        res.render('index.ejs', {tasks:tasks})
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const readFile = (filename) => {
+    return new Promise((resolve, reject) => {
+        // get data from file
+        fs.readFile(filename, 'utf8', (err, data) => {
+            if (err) {
+                console.error(err)
+                return;
+            }
+            // tasks list data from file
+            const tasks = data.split('\n')
+            resolve(tasks)
+        })
+        
     })
+}
+
+app.get('/', (req, res) => {
+    readFile('./tasks')
+    .then(tasks => {
+        res.render('index.ejs', {tasks: tasks})
+    })
+})
+
+app.post('/', (req, res) => {
+    readFile('./tasks')
+        .then(tasks => {
+            tasks.push(req.body.task)
+            const data = tasks.join('\n')
+            fs.writeFile('./tasks', data, err => {
+                if (err) {
+                    console.error(err)
+                    return;
+                }
+                res.redirect('/')
+            })
+        })
 })
 
 app.listen(3000, () => {
