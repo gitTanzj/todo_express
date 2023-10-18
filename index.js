2fc45bfb2f734f237e2afc2a661b819ef0078ed1
gitTanzj <kalle.riit@gmail.com>
2023-10-18 13:11:12 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 const path = require('path')
 app.set('view-engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    fs.readFile('./tasks', 'utf8', (err, data) => {
+        if (err) {
+            console.err(err)
+            return;
+        }
+        const tasks = data.split('\n')
+        res.render('index.ejs', {tasks:tasks})
+    })
 })
 
 app.listen(3000, () => {
